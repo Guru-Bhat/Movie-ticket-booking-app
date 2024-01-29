bd737aae49fc50e82aff446b6ead18a4cd457703
Guruprasad Bhat <142134599+Guru-Bhat@users.noreply.github.com>
2024-01-29 12:29:26 +0530

    msg chnage

--- src/Pages/Confirmation.js
+++ src/Pages/Confirmation.js
@@ -2,7 +2,7 @@ export default function Confirmation({ onNext, seatSelection }) {
     return (
       <div className="App">
         <h1>Hello Confirmation</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h2>Start now...</h2>
       </div>
     );
   }
